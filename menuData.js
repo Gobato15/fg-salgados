d761e20ab5a34981e3b7fc945e8cca460ad74b3f
Gobato15 <gobato59@gmail.com>
2026-09-20 19:52:46 -0300

    Alinha cardapio do site com os 9 produtos do banco

--- menuData.js
+++ menuData.js
@@ -1,83 +1,83 @@
 const fgMenuItems = [
     {
-        id: "p1",
-        name: "Coxinha de Frango",
-        category: "fritos",
-        price: 6,
-        description: "Coxinha de frango (100g) com massa artesanal de batata, receita da casa. Pronta para airfryer!",
-        image: "images/ags_coxinha.webp",
-        units: 1
-    },
-    {
-        id: "p2",
-        name: "Croissant Presunto e Queijo",
-        category: "assados",
-        price: 6,
-        description: "Croissant recheado com presunto fatiado e queijo derretido. Clássico folhado crocante.",
-        image: "images/ags_croissant.webp",
-        units: 1
-    },
-    {
-        id: "p3",
-        name: "Bauru de Presunto e Queijo",
-        category: "assados",
-        price: 6,
-        description: "Bauru com massa fofinha e recheio generoso de presunto, queijo e orégano.",
-        image: "images/ags_bauru.webp",
-        units: 1
-    },
-    {
-        id: "p4",
-        name: "X-Bacon",
+        id: "1",
+        name: "X Carne",
         category: "burgers",
-        price: 7.3,
-        description: "X-Bacon com carne, bacon crocante, cheddar e molho especial.",
+        price: 10,
+        description: "Carne suculenta de 56g, mussarela derretida, presunto, tomate, orégano no pão macio.",
+        image: "images/x_carne.webp",
+        units: 1
+    },
+    {
+        id: "2",
+        name: "X Picanha",
+        category: "burgers",
+        price: 11,
+        description: "Carne de picanha de 56g, presunto, mussarela e catupiry cremoso.",
+        image: "images/x_picanha.webp",
+        units: 1
+    },
+    {
+        id: "3",
+        name: "Duplo Cheddar",
+        category: "burgers",
+        price: 10,
+        description: "Pão macio, duas carnes suculentas de 56g e cheddar delicioso.",
+        image: "images/duplo_cheddar.webp",
+        units: 1
+    },
+    {
+        id: "4",
+        name: "Coxinha de Frango",
+        category: "coxinha",
+        price: 9,
+        description: "Coxinha de frango cremosa de 100g, feita com massa artesanal de batata. Receita da casa.",
+        image: "images/coxinha_premium.webp",
+        units: 1
+    },
+    {
+        id: "5",
+        name: "Croissant Presunto e Queijo",
+        category: "croissant",
+        price: 10,
+        description: "O clássico folhado recheado com presunto fatiado e queijo derretido.",
+        image: "images/croissant_presunto_queijo_premium.webp",
+        units: 1
+    },
+    {
+        id: "6",
+        name: "Lanche Natural de Frango",
+        category: "lanches",
+        price: 9,
+        description: "Lanche natural com frango desfiado, maionese, alface e cenoura ralada no pão de forma macio.",
+        image: "images/lanche_natural_frango.webp",
+        units: 1
+    },
+    {
+        id: "7",
+        name: "Bauru de Presunto e Queijo",
+        category: "salgados",
+        price: 9,
+        description: "Clássico bauru com massa fofinha e recheio generoso de presunto e queijo derretido, temperado com um toque de orégano.",
+        image: "images/bauru_queijo_presunto.webp",
+        units: 1
+    },
+    {
+        id: "8",
+        name: "X Bacon",
+        category: "burgers",
+        price: 10,
+        description: "Carne de 56g, bacon crocante, mussarela derretida e maionese da casa no pão macio.",
         image: "images/fg_xbacon.webp",
         units: 1
     },
     {
-        id: "p10",
-        name: "X-Cheddar",
+        id: "9",
+        name: "X AGS",
         category: "burgers",
-        price: 8.3,
-        description: "X-Cheddar com duas camadas de cheddar derretido e carne suculenta.",
-        image: "images/fg_xcheddar.webp",
-        units: 1
-    },
-    {
-        id: "p11",
-        name: "X-Picanha",
-        category: "burgers",
-        price: 8.3,
-        description: "X-Picanha com carne de picanha, catupiry cremoso e queijo.",
-        image: "images/fg_xpicanha.webp",
-        units: 1
-    },
-    {
-        id: "p9",
-        name: "Croissant de Queijo",
-        category: "assados",
-        price: 6,
-        description: "Croissant folhado crocante com queijo cremoso e derretido.",
-        image: "images/ags_croissant_queijo.webp",
-        units: 1
-    },
-    {
-        id: "p16",
-        name: "Bolo Salgado de Frango c/ Requeijão",
-        category: "assados",
-        price: 10,
-        description: "Bolo salgado de frango com requeijão cremoso e azeitonas gratinados.",
-        image: "images/ags_bolo_salgado.webp",
-        units: 1
-    },
-    {
-        id: "p17",
-        name: "Torta de Frango Caseira",
-        category: "assados",
-        price: 15,
-        description: "Torta de frango com massa caseira derretendo na boca.",
-        image: "images/ags_torta_frango.webp",
+        price: 14,
+        description: "Pão macio, duas carnes suculentas de 56g, tomate, presunto, queijo, requeijão, calabresa e bacon.",
+        image: "images/x_ags_premium.webp",
         units: 1
     }
 ]
